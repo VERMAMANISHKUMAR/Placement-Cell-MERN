5f159a69721ae674f5dc483c63b657aed4724f5c
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2025-02-21 15:46:09 +0530

    Update Register.js
    backend url add

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3808/api/auth/register", {
+      const response = await fetch("https://placement-cell-mern-backend.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
