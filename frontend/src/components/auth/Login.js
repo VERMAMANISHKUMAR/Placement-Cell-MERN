0b147970209e043a8a80ca9bfb2fe130d4ca2fe5
Manish Kumar Verma <157243115+VERMAMANISHKUMAR@users.noreply.github.com>
2025-02-21 15:45:27 +0530

    Update Login.js
    backend url add

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3808/api/auth/login", {
+      const response = await fetch("https://placement-cell-mern-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
